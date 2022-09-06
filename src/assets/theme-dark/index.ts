220faa0412ac8c39a9e83c21b1a06633c99a3aeb
vijaykolar <vijayikolar@gmail.com>
2022-09-06 09:38:59 +0530

    Changed path

--- src/assets/theme-dark/index.ts
+++ src/assets/theme-dark/index.ts
@@ -2,9 +2,9 @@
 import { createTheme } from "@mui/material/styles";
 
 // React TS Base Styles
-import colors from "assets/theme-dark/base/colors";
-import breakpoints from "assets/theme-dark/base/breakpoints";
-import typography from "assets/theme-dark/base/typography";
+import colors from "./base/colors";
+import breakpoints from "./base/breakpoints";
+import typography from "./base/typography";
 
 export default createTheme({
   breakpoints: { ...breakpoints },
