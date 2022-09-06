b0e3476a4432a3e2090e6ed6511a94308539fe0e
vijaykolar <vijayikolar@gmail.com>
2022-09-06 09:40:50 +0530

    Changed message

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-// Material Dashboard 2 PRO React TS Context Provider
+// React context Provider
 import { MaterialUIControllerProvider } from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
