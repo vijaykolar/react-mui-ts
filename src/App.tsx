dcb9401ff18239819cd8c498eee1ad3d3f80dbab
vijaykolar <vijayikolar@gmail.com>
2022-09-07 09:04:37 +0530

    Working on creating MUI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, CssBaseline, Paper, ThemeProvider } from "@mui/material";
+import { CssBaseline, Paper, ThemeProvider } from "@mui/material";
 
 // React contexts
 import { useMaterialUIController } from "./context";
@@ -8,14 +8,26 @@ import theme from "./assets/theme";
 
 // React Dark Mode theme
 import themeDark from "./assets/theme-dark";
+import MDButton from "components/MDButton";
 
 function App() {
   const [controller, dispatch] = useMaterialUIController();
   const { darkMode } = controller;
   return (
     <ThemeProvider theme={darkMode ? themeDark : theme}>
-      <Button color="success">Submit</Button>
       <Paper>sdasd</Paper>
+      <MDButton variant="text" color="success">
+        Props
+      </MDButton>
+      <MDButton variant="outlined" color="secondary">
+        Props
+      </MDButton>
+      <MDButton variant="gradient" color="warning">
+        Props d
+      </MDButton>
+      <MDButton variant="text" color="error">
+        Props
+      </MDButton>
       <CssBaseline />
     </ThemeProvider>
   );
